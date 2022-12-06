54a64d4f5714eddfe54f6603fc53d3896df0d7ab
bluet <bluet@MSI>
2022-12-06 14:58:47 +0300

    fixed

---
+++ BlazorApp9/Components/ARSelectItem.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp9.Components
+{
+    public partial class ARSelectItem<TValue>
+    {
+        [Parameter] public TValue Value { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+    }
+}
---
+++ BlazorApp9/Components/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp9.Components
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
